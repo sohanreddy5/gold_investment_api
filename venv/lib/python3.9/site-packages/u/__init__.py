c6dcaedfe69184b43a1e8d8d8420e591c23b4636
sohan <msohanreddy@gmail.com>
2025-09-06 20:47:18 +0530

    Clean repo: removed venv, DB, and system files

--- venv/lib/python3.9/site-packages/u/__init__.py
+++
@@ -1,9 +0,0 @@
-__version__ = "1.0.4"
-
-from .prefixes import *
-from .quantity import *
-from .capital_quantities import *
-from .unit import *
-
-# This needs to be last to avoid circular import errors
-from .quantities import *
